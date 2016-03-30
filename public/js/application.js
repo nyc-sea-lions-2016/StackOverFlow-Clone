f9a98ec17dd073a67d58a511216bf1877f59b13c
Brian Bier & Alex Forger <pair+brianbier.darrow87@devbootcamp.com>
2016-03-30 18:42:56 -0400

    add jquery to show comment form and hide comment button when comment button is clicked

--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
-  ('#new_comment_button').on('click', function(event){
+  $('#new_comment_button').on('click', function(event){
     event.preventDefault();
+    $('#new_comment_form').show()
+    // $('#new_comment_button').addClass('hidden')
+    $(this).hide()
 
   })
 });
