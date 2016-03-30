85022bdef632b203130d4d5cafbb1cc48c5c33e9
Brian Bier & Alex Forger <pair+brianbier.darrow87@devbootcamp.com>
2016-03-30 15:59:57 -0400

    add new question form and errors

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,7 +4,6 @@ class Answer < ActiveRecord::Base
   has_many :votes, as: :votable
   has_many :comments, as: :commentable
 
-  validates :title, presence: true
   validates :content, presence: true
   accepts_nested_attributes_for :comments
   accepts_nested_attributes_for :votes
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ActiveRecord::Base
   has_many :comments, as: :commentable
 
   validates :email, presence: true, uniqueness: true
-  validates :name, presence: true
-  validates :password, presence: true
+  validates :username, presence: true
+  validates :password_digest, presence: true
 end
