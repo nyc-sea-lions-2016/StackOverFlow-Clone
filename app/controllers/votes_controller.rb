51fd6712586e5d08e6bdf508f29fd4f2eaca72f5
Brian Bier <briansbier@gmail.com>
2016-03-31 16:14:47 -0400

    Fix button problem for user votes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,7 +2,7 @@ get '/votes/:id' do
   if logged_in?
     @question = Question.find(params[:id])
     @question.votes.create(value: 1)
-    redirect '/'
+    redirect "questions/#{@question.id}"
   else
     #Error not going through
     @errors = 'You need to log in'
@@ -14,7 +14,7 @@ get '/votes/reduce/:id' do
   if logged_in?
   @question = Question.find(params[:id])
   @question.votes.create(value: - 1)
-  redirect '/'
+  redirect "questions/#{@question.id}"
   else
     redirect '/'
   end
